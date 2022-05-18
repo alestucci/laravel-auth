dcd898c91ba5fccb828b949e4c05aece657ce1af
alestucci <alessandrotucci@gmail.com>
2022-05-18 16:43:10 +0200

    Seeder

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,28 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'content',
+        'slug'
+    ];
+
+    static public function titleToSlug($string) {
+        $slug = Str::of($string)->slug('-');
+        $_i = '0';
+        while(self::where('slug', $slug)->first()) {
+            if ($_i === 0) {
+                $_i = '';
+                return $_i;
+            }
+            $slug = "$slug-$_i";
+            $_i++;
+        }
+        return $slug;
+
+    }
 }
